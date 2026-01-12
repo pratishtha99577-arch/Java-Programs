0ed1b27b896e7a33a2f9d003eeb3fb04ffb7d5a2
Pratishtha <pratishtha99577@gmail.com>
2026-01-12 19:33:13 +0530

    Create InputName.java
    Add program to take name as input.

---
+++ Basics/InputName.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class InputName {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
+
+    } 
+    
+}
