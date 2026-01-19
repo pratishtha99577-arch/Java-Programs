f89080e3dbc2bd91568ba213f570f62f57f1fa6c
Pratishtha <pratishtha99577@gmail.com>
2026-01-19 21:09:34 +0530

    Create MultiplicationTable.java
    A Java program that accepts a number from the user and displays its multiplication table using a for loop.

---
+++ Basics/MultiplicationTable.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class MultiplicationTable {
+    public static void main(String agrs[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= 10; i++){
+          System.out.println(n*i); 
+        }
+
+    
+    }
+
+
+    
+}
+
