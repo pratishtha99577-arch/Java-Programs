9db4118acde790fa5835d2d441b63f64c16d0a3e
Pratishtha <pratishtha99577@gmail.com>
2026-01-20 19:12:26 +0530

    Create SolidSquarePattern.java
    Java program to print a solid square pattern using nested for loops.

---
+++ Basics/SolidSquarePattern.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class SolidSquarePattern {
+    public static void main(String agrs[]){
+        Scanner sc = new Scanner(System.in);
+        int n = 4;
+        int m = 5;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j<=m; j++){
+                System.out.print("*");
+            }
+          System.out.println(); 
+        }
+
+    
+    }
+
+
+    
+}
+
+
