4ebf0529e31263c080c1025de4f823c61c4cac32
Pratishtha <pratishtha99577@gmail.com>
2026-01-22 20:48:35 +0530

    Create HalfTrianglePattern.java
    Java program to print a half triangle pattern using nested for loops.

---
+++ Basics/HalfTrianglePattern.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class HalfTrianglePattern {
+    public static void main(String agrs[]){
+
+        int n = 4;
+        
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j<=i; j++){
+                
+             System.out.print("*");
+                
+                
+            }
+          System.out.println(); 
+        }
+
+    
+    }
+
+
+    
+}
+
