9b1c1672c8ba5cc2d8cea9a2a5f5fbd139c79a71
Pratishtha <pratishtha99577@gmail.com>
2026-01-23 18:34:12 +0530

    Create InvertedHalfTrianglePattern.java
    Java program to print an inverted half triangle pattern using nested for loops.

---
+++ Basics/InvertedHalfTrianglePattern.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class InvertedHalfTrianglePattern {
+    public static void main(String agrs[]){
+
+        int n = 4;
+        
+
+        for(int i = n; i >= 1; i--){
+            for(int j = 1; j<=i; j++){
+                
+             System.out.print("*");
+                
+                
+            }
+          System.out.println(); 
+        }
+
+    
+    }
+
+
+    
+}
+
+
