48a2c4a724dec25130d02395adcf254032df0756
Pratishtha <pratishtha99577@gmail.com>
2026-01-25 12:14:57 +0530

    Add number half triangle pattern program
    A Java program that generates a half triangle number pattern using nested loops.

---
+++ Basics/HalfTriangleNumberPattern.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class HalfTriangleNumberPattern {
+    public static void main(String agrs[]){
+
+        int n = 5;
+        
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j<=i; j++){
+                
+             System.out.print(j+" ");
+                
+            }
+
+        
+          System.out.println(); 
+        }
+
+    
+    }
+
+
+    
+}
+
+
+
+
