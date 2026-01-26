3557eb2915e3ebd765dc8bd65713470930c782f1
Pratishtha <pratishtha99577@gmail.com>
2026-01-26 11:39:16 +0530

    Create InvertedNumberTriangle.java
    Java program to print inverted number triangle using for loop.

---
+++ Basics/InvertedNumberTriangle.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class InvertedNumberTriangle {
+    public static void main(String agrs[]){
+
+        int n = 5;
+        
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j<=n-i+1; j++){
+                
+             System.out.print(j);
+                
+            }
+
+        
+          System.out.println(); 
+        }
+
+    
+    }
+
+
+    
+}
+
+
+
+
+
