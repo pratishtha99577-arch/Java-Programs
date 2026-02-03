c01c20d3d6ba8f322d05a902289a30539f6dfb04
Pratishtha <pratishtha99577@gmail.com>
2026-02-03 19:53:15 +0530

    Create MultiplyTwoNumbersUsingFunction.java
    Java program to take two numbers as input and calculate their product using a user-defined function.

---
+++ Basics/MultiplyTwoNumbersUsingFunction.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class MultiplyTwoNumbersUsingFunction {
+    public static int calculateProduct(int a, int b){
+        return a * b;
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("Product of two numbers is : "+ calculateProduct(a , b));
+
+
+    }
+}
