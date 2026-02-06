64cfb258f99b9c15758e8fb33a330a4678a03839
Pratishtha <pratishtha99577@gmail.com>
2026-02-06 22:10:00 +0530

    Added hollow rhombus pattern program in Java
    Java program to print a hollow rhombus pattern using nested loops.

---
+++ Basics/HollowRhombus.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class HollowRhombus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            // Leading spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Stars and spaces
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
