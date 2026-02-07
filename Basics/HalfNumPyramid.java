91f9d6ba7350eee6b6be0fbcaffa5ac937891b90
Pratishtha <pratishtha99577@gmail.com>
2026-02-07 13:52:22 +0530

    Create HalfNumPyramid.java
    Java program to print a half number pyramid pattern using nested loops and user input.

---
+++ Basics/HalfNumPyramid.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class HalfNumPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            // Spaces at the beginning
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+
+            // Numbers
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j+" ");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
