99fb1608ce2074d05d9c0170d7f184aca8696b5f
Pratishtha <pratishtha99577@gmail.com>
2026-02-07 14:10:34 +0530

    Added inverted half pyramid pattern program
    Java program to print an inverted half pyramid pattern using nested loops.

---
+++ Basics/InvertedHalfPyramid.java
@@ -0,0 +1,16 @@
+public class InvertedHalfPyramid {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            // Print same number i, (n - i + 1) times
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(i);
+            }
+
+            System.out.println();
+        }
+    }
+}
