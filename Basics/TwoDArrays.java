6a2ea7bc06948befe893a752af6bed6fb83091ac
Pratishtha <pratishtha99577@gmail.com>
2026-02-13 19:47:54 +0530

    Create TwoDArrays.java
    A Java program to input and display elements of a 2D array.

---
+++ Basics/TwoDArrays.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class TwoDArrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int rows = sc.nextInt();
+        System.out.println("Enter the number of columns:");
+        int cols = sc.nextInt();
+        System.out.println("Enter the elements of the array:");
+        int[][] numbers = new int[rows][cols];
+
+        //input
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("The elements of the array are:");
+
+        //output
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+    
+}    
