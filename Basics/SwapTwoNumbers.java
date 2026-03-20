7d6ca886900e05b00bd43eb90c39aac27333c929
Pratishtha <pratishtha99577@gmail.com>
2026-03-20 20:16:30 +0530

    Create SwapTwoNumbers.java
    A simple Java Program to Swap two numbers using third variable.

---
+++ Basics/SwapTwoNumbers.java
@@ -0,0 +1,13 @@
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        int a = 99;
+        int b = 88;
+        
+        int temp = a;
+        a = b;
+        b = temp;
+         
+        System.out.println("The value of a and b is: " + a + " " + b);
+    }
+    
+}
