d54bb95c1e4236b07309edd05e3247b92f7926ea
Pratishtha <pratishtha99577@gmail.com>
2026-04-02 16:41:00 +0530

    Create FrequencyArray.java

---
+++ Basics/FrequencyArray.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class FrequencyArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        boolean visited[] = new boolean[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            visited[i] = false;
+        }
+
+        System.out.println("Element\tFrequency");
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i])
+                continue;
+
+            int count = 1;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    visited[j] = true;
+                }
+            }
+
+            System.out.println(arr[i] + "\t" + count);
+        }
+    }
+}
