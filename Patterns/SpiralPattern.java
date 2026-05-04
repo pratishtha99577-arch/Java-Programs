e40b4fb847d2460a098220e154dd026e0f26f83e
Pratishtha <pratishtha99577@gmail.com>
2026-05-04 21:04:47 +0530

    Create SpiralPattern.java

---
+++ Patterns/SpiralPattern.java
@@ -0,0 +1,37 @@
+public class SpiralPattern {
+    public static void main(String[] args) {
+        int n = 4;
+        int matrix[][] = new int[n][n];
+
+        int top = 0, bottom = n - 1, left = 0, right = n - 1;
+        int num = 1;
+
+        while (true) {
+            for (int i = left; i <= right; i++)
+                matrix[top][i] = num++;
+            top++;
+            if (top > bottom) break;
+
+            for (int i = top; i <= bottom; i++)
+                matrix[i][right] = num++;
+            right--;
+            if (left > right) break;
+
+            for (int i = right; i >= left; i--)
+                matrix[bottom][i] = num++;
+            bottom--;
+            if (top > bottom) break;
+
+            for (int i = bottom; i >= top; i--)
+                matrix[i][left] = num++;
+            left++;
+            if (left > right) break;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                System.out.print(matrix[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
